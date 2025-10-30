dbaebf5cd017d076b123a4bc8687d0d6c1052076
peasee <98815791+peasee@users.noreply.github.com>
2025-10-30 14:30:55 +1000

    wip

--- vortex-datafusion/src/persistent/source.rs
+++ vortex-datafusion/src/persistent/source.rs
@@ -227,6 +227,8 @@ impl FileSource for VortexSource {
         filters: Vec<Arc<dyn PhysicalExpr>>,
         _config: &ConfigOptions,
     ) -> DFResult<FilterPushdownPropagation<Arc<dyn FileSource>>> {
+        println!("Pushing down filters to VortexSource: {filters:?}");
+
         let Some(schema) = self.arrow_file_schema.as_ref() else {
             return Ok(FilterPushdownPropagation::with_parent_pushdown_result(
                 vec![PushedDown::No; filters.len()],
@@ -254,6 +256,8 @@ impl FileSource for VortexSource {
                 }
             })
             .collect::<Vec<_>>();
+
+        println!("Supported filters: {supported_filters:?}");
 
         if supported_filters
             .iter()
