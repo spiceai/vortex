773575e3da9fe5c82a3fd607ce7582176084d402
Luke Kim <80174+lukekim@users.noreply.github.com>
2026-01-14 16:34:18 -0800

    fix: Update persistent module to use simplified expression handling from PR #8

--- vortex-datafusion/src/persistent/source.rs
+++ vortex-datafusion/src/persistent/source.rs
@@ -3,28 +3,25 @@
 
 use std::any::Any;
 use std::fmt::Formatter;
-use std::sync::Arc;
-use std::sync::Weak;
+use std::sync::{Arc, Weak};
 
-use datafusion_common::Result as DFResult;
-use datafusion_common::Statistics;
+use arrow_schema::SchemaRef;
 use datafusion_common::config::ConfigOptions;
-use datafusion_datasource::TableSchema;
+use datafusion_common::{Result as DFResult, Statistics};
 use datafusion_datasource::file::FileSource;
 use datafusion_datasource::file_scan_config::FileScanConfig;
 use datafusion_datasource::file_stream::FileOpener;
-use datafusion_datasource::schema_adapter::DefaultSchemaAdapterFactory;
-use datafusion_datasource::schema_adapter::SchemaAdapterFactory;
-use datafusion_physical_expr::PhysicalExprRef;
-use datafusion_physical_expr::conjunction;
-use datafusion_physical_expr_adapter::PhysicalExprAdapterFactory;
+use datafusion_datasource::schema_adapter::{DefaultSchemaAdapterFactory, SchemaAdapterFactory};
+use datafusion_physical_expr::{PhysicalExprRef, conjunction};
+use datafusion_physical_expr_adapter::{
+    DefaultPhysicalExprAdapterFactory, PhysicalExprAdapterFactory,
+};
 use datafusion_physical_expr_common::physical_expr::fmt_sql;
-use datafusion_physical_plan::DisplayFormatType;
-use datafusion_physical_plan::PhysicalExpr;
-use datafusion_physical_plan::filter_pushdown::FilterPushdownPropagation;
-use datafusion_physical_plan::filter_pushdown::PushedDown;
-use datafusion_physical_plan::filter_pushdown::PushedDownPredicate;
+use datafusion_physical_plan::filter_pushdown::{
+    FilterPushdownPropagation, PushedDown, PushedDownPredicate,
+};
 use datafusion_physical_plan::metrics::ExecutionPlanMetricsSet;
+use datafusion_physical_plan::{DisplayFormatType, PhysicalExpr};
 use object_store::ObjectStore;
 use object_store::path::Path;
 use vortex::error::VortexExpect as _;
@@ -37,10 +34,7 @@ use vortex_utils::aliases::dash_map::DashMap;
 use super::cache::VortexFileCache;
 use super::metrics::PARTITION_LABEL;
 use super::opener::VortexOpener;
-use crate::convert::exprs::DefaultExpressionConvertor;
-use crate::convert::exprs::ExpressionConvertor;
 use crate::convert::exprs::can_be_pushed_down;
-use crate::vendor::schema_rewriter::DF52PhysicalExprAdapterFactory;
 
 /// Execution plan for reading one or more Vortex files, intended to be consumed by [`DataSourceExec`].
 ///
@@ -57,7 +51,8 @@ pub struct VortexSource {
     pub(crate) vortex_predicate: Option<PhysicalExprRef>,
     pub(crate) batch_size: Option<usize>,
     pub(crate) projected_statistics: Option<Statistics>,
-    pub(crate) table_schema: Option<TableSchema>,
+    /// This is the file schema the table expects, which is the table's schema without partition columns, and **not** the file's physical schema.
+    pub(crate) arrow_file_schema: Option<SchemaRef>,
     pub(crate) schema_adapter_factory: Option<Arc<dyn SchemaAdapterFactory>>,
     pub(crate) expr_adapter_factory: Option<Arc<dyn PhysicalExprAdapterFactory>>,
     _unused_df_metrics: ExecutionPlanMetricsSet,
@@ -65,7 +60,6 @@ pub struct VortexSource {
     ///
     /// Sharing the readers allows us to only read every layout once from the file, even across partitions.
     layout_readers: Arc<DashMap<Path, Weak<dyn LayoutReader>>>,
-    expression_convertor: Arc<dyn ExpressionConvertor>,
 }
 
 impl VortexSource {
@@ -77,22 +71,25 @@ impl VortexSource {
             vortex_predicate: None,
             batch_size: None,
             projected_statistics: None,
-            table_schema: None,
+            arrow_file_schema: None,
             schema_adapter_factory: None,
             expr_adapter_factory: None,
             _unused_df_metrics: Default::default(),
             layout_readers: Arc::new(DashMap::default()),
-            expression_convertor: Arc::new(DefaultExpressionConvertor::default()),
         }
     }
 
-    /// Set a [`ExpressionConvertor`] to control how Datafusion expression should be converted and pushed down.
-    pub fn with_expression_convertor(
-        mut self,
-        expr_convertor: Arc<dyn ExpressionConvertor>,
-    ) -> Self {
-        self.expression_convertor = expr_convertor;
-        self
+    /// Sets a [`PhysicalExprAdapterFactory`] for the [`VortexSource`].
+    /// Currently, this must be provided in order to filter columns in files that have a different data type from the unified table schema.
+    ///
+    /// This factory will take precedence when opening files over instances provided by the [`FileScanConfig`].
+    pub fn with_expr_adapter_factory(
+        &self,
+        expr_adapter_factory: Arc<dyn PhysicalExprAdapterFactory>,
+    ) -> Arc<dyn FileSource> {
+        let mut source = self.clone();
+        source.expr_adapter_factory = Some(expr_adapter_factory);
+        Arc::new(source)
     }
 }
 
@@ -116,13 +113,6 @@ impl FileSource for VortexSource {
             .expr_adapter_factory
             .as_ref()
             .or(base_config.expr_adapter_factory.as_ref());
-
-        if expr_adapter.is_some() {
-            tracing::warn!(
-                "Schema evolution with VortexSource may not work as expected if you override the adapter."
-            );
-        }
-
         let schema_adapter = self.schema_adapter_factory.as_ref();
 
         // This match is here to support the behavior defined by [`ListingTable`], see https://github.com/apache/datafusion/issues/16800 for more details.
@@ -139,14 +129,12 @@ impl FileSource for VortexSource {
                 (None, schema_adapter.clone())
             }
             (None, None) => (
-                Some(Arc::new(DF52PhysicalExprAdapterFactory) as _),
+                Some(Arc::new(DefaultPhysicalExprAdapterFactory) as _),
                 Arc::new(DefaultSchemaAdapterFactory) as _,
             ),
         };
 
         let projection = base_config.file_column_projection_indices().map(Arc::from);
-
-        let table_schema = base_config.table_schema.clone();
 
         let opener = VortexOpener {
             session: self.session.clone(),
@@ -156,14 +144,14 @@ impl FileSource for VortexSource {
             file_pruning_predicate: self.full_predicate.clone(),
             expr_adapter_factory,
             schema_adapter_factory,
-            table_schema,
+            partition_fields: base_config.table_partition_cols.clone(),
+            logical_schema: base_config.file_schema.clone(),
             file_cache: self.file_cache.clone(),
             batch_size,
             limit: base_config.limit,
             metrics: partition_metrics,
             layout_readers: self.layout_readers.clone(),
             has_output_ordering: !base_config.output_ordering.is_empty(),
-            expression_convertor: Arc::new(DefaultExpressionConvertor::default()),
         };
 
         Arc::new(opener)
@@ -179,9 +167,9 @@ impl FileSource for VortexSource {
         Arc::new(source)
     }
 
-    fn with_schema(&self, schema: TableSchema) -> Arc<dyn FileSource> {
+    fn with_schema(&self, schema: SchemaRef) -> Arc<dyn FileSource> {
         let mut source = self.clone();
-        source.table_schema = Some(schema);
+        source.arrow_file_schema = Some(schema);
         Arc::new(source)
     }
 
@@ -248,7 +236,7 @@ impl FileSource for VortexSource {
             ));
         }
 
-        let Some(table_schema) = self.table_schema.as_ref() else {
+        let Some(schema) = self.arrow_file_schema.as_ref() else {
             return Ok(FilterPushdownPropagation::with_parent_pushdown_result(
                 vec![PushedDown::No; filters.len()],
             ));
@@ -268,7 +256,7 @@ impl FileSource for VortexSource {
         let supported_filters = filters
             .into_iter()
             .map(|expr| {
-                if can_be_pushed_down(&expr, table_schema.file_schema()) {
+                if can_be_pushed_down(&expr, schema) {
                     PushedDownPredicate::supported(expr)
                 } else {
                     PushedDownPredicate::unsupported(expr)
