a6e8e8ae43fda6a3cf21d7cbccf37b9af5ae2a7d
Joe Isaacs <joe.isaacs@live.co.uk>
2026-02-10 12:47:57 +0000

    feat[expr]: new fill null expr (#6385)
    Creates an expression that replaces null values with a fill value.
    
    ```
    # use vortex_array::expr::{fill_null, root, lit};
    let expr = fill_null(root(), lit(0i32));
    ```
    
    Signed-off-by: Joe Isaacs <joe.isaacs@live.co.uk>

--- vortex-array/src/expr/session.rs
+++ vortex-array/src/expr/session.rs
@@ -10,6 +10,7 @@ use crate::expr::exprs::between::Between;
 use crate::expr::exprs::binary::Binary;
 use crate::expr::exprs::case_when::CaseWhen;
 use crate::expr::exprs::cast::Cast;
+use crate::expr::exprs::fill_null::FillNull;
 use crate::expr::exprs::get_item::GetItem;
 use crate::expr::exprs::is_null::IsNull;
 use crate::expr::exprs::like::Like;
@@ -58,6 +59,7 @@ impl Default for ExprSession {
             ExprVTable::new_static(&Binary),
             ExprVTable::new_static(&CaseWhen),
             ExprVTable::new_static(&Cast),
+            ExprVTable::new_static(&FillNull),
             ExprVTable::new_static(&GetItem),
             ExprVTable::new_static(&IsNull),
             ExprVTable::new_static(&Like),
