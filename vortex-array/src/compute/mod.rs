0ec500da01c41ddcb3f4dc3fd96366666e03c660
Joe Isaacs <joe.isaacs@live.co.uk>
2026-02-13 15:19:23 +0000

    feat[array]: refactor `zip` compute kernel to lazy `zip` via ScalarFn Array (#6461)
    We want lazy compute for expressions, this PR moves over zip
    
    see: https://github.com/vortex-data/vortex/discussions/5921
    
    ---------
    
    Signed-off-by: Joe Isaacs <joe.isaacs@live.co.uk>

--- vortex-array/src/compute/mod.rs
+++ vortex-array/src/compute/mod.rs
@@ -46,14 +46,6 @@ pub use zip::*;
 use crate::Array;
 use crate::ArrayRef;
 use crate::builders::ArrayBuilder;
-pub use crate::expr::FillNullExecuteAdaptor;
-pub use crate::expr::FillNullKernel;
-pub use crate::expr::FillNullReduce;
-pub use crate::expr::FillNullReduceAdaptor;
-pub use crate::expr::NotExecuteAdaptor;
-pub use crate::expr::NotKernel;
-pub use crate::expr::NotReduce;
-pub use crate::expr::NotReduceAdaptor;
 
 #[cfg(feature = "arbitrary")]
 mod arbitrary;
@@ -100,7 +92,6 @@ pub fn warm_up_vtables() {
     min_max::warm_up_vtable();
     nan_count::warm_up_vtable();
     sum::warm_up_vtable();
-    zip::warm_up_vtable();
 }
 
 impl ComputeFn {
